a9df3dae57a9b3b35217bd81c065f5f45ecb7c38
OrghoN <anoronyo@gmail.com>
2017-02-20 12:35:41 -0600

    set default editor as emacs

--- settingsNS.bash
+++ settingsNS.bash
@@ -14,6 +14,9 @@ echo "Set up Git"
 git config --global user.name "OrghoN"
 git config --global user.email "anoronyo@gmail.com"
 
+echo "Set emacs as default editor"
+echo 'export EDITOR="emacs -nw"' >> ~/.bashrc
+
 #list of atom packages
 packages=( jsdoc-generator atom-easy-jsdoc latex language-latex atom-hack autocomplete-hack highlight-selected language-hack language-vhdl markdown-scroll-sync minimap-highlight-selected threejs-snippets tidy-markdown todo-show minimap autoclose-html color-picker atom-beautify linter file-icons pretty-json markdown-preview-plus markdown-pdf language-hdl )
 
