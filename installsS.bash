8d1f989a83a8f041b27b75dd2435b8cd848a8618
OrghoN <anoronyo@gmail.com>
2017-02-20 13:09:43 -0600

    added test packages for install

---
+++ installsS.bash
@@ -0,0 +1,21 @@
+#list of ppas
+ppas=( ppa:webupd8team/atom )
+
+echo "add PPA's:"
+#adding PPA's
+for i in "${ppas[@]}"; do
+    add-apt-repository -y $i
+    echo $i
+done
+
+apt update
+
+#list of applications
+packages=( atom chromium-browser )
+
+echo "add PPA's:"
+#adding PPA's
+for i in "${packages[@]}"; do
+    apt install -y $i
+    echo $i
+done
